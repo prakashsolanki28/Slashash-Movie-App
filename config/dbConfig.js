46dd171c115de7aa4ad816047dfdc0c3ed9ac961
Prakash Solanki <prakashs.mca22@mbm.ac.in>
2024-06-17 15:03:06 +0530

    Setup project

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Mysql@2024',
+    password: '',
     database: 'my_movies_db'
 });
 
